1f929da8c8f0c01593d5f4ac9219ece50bb0985d
Cassio Cabral <cassioscabral@gmail.com>
2015-12-21 16:58:37 -0300

    change doublo quotes to single

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ new WebpackDevServer(webpack(config), config.devServer)
   if (err) {
     console.log(err);
   }
+
   console.log('Listening at localhost:' + config.port);
   console.log('Opening your system browser...');
   open('http://localhost:' + config.port + '/webpack-dev-server/');
