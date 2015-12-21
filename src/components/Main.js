9b4178c138b7a19848cc1183cedc263fb99983b7
Cassio Cabral <cassioscabral@gmail.com>
2015-12-21 16:58:56 -0300

    change doublo quotes to single

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,9 +8,9 @@ let yeomanImage = require('../images/yeoman.png');
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className='index'>
+        <img src={yeomanImage} alt='Yeoman Generator' />
+        <div className='notice'>Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
   }
