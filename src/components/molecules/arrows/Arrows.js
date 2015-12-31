c5881705933039ca121548482205c5e169cd40be
Cassio Cabral <cassioscabral@gmail.com>
2015-12-31 18:38:23 -0300

    add arrows component

---
+++ src/components/molecules/arrows/Arrows.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('./stylesheets/arrows.scss');
+
+class Arrows extends React.Component {
+  render() {
+    return (
+      <div className='arrows-component'>
+        Please edit src/components/molecules/arrows/Arrows.js to update this component!
+      </div>
+    );
+  }
+}
+
+Arrows.displayName = 'MoleculeArrows';
+
+// Uncomment properties you need
+// Arrows.propTypes = {};
+// Arrows.defaultProps = {};
+
+export default Arrows;
