b982379cd8f4b338d96626b2a31f7841fc9aab96
Cassio Cabral <cassioscabral@gmail.com>
2015-12-30 22:15:59 -0300

    add tab component

---
+++ src/components/atoms/tab/Tab.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('./stylesheets/tab.scss');
+
+class Tab extends React.Component {
+  render() {
+    return (
+      <div className='tab-component'>
+        Please edit src/components/atoms/tab/Tab.js to update this component!
+      </div>
+    );
+  }
+}
+
+Tab.displayName = 'AtomTab';
+
+// Uncomment properties you need
+// Tab.propTypes = {};
+// Tab.defaultProps = {};
+
+export default Tab;
