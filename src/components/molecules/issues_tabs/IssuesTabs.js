8021e78379a421b56e1cc5f5bbd4e6561c6454cd
Cassio Cabral <cassioscabral@gmail.com>
2015-12-30 22:15:36 -0300

    add issues tabs component

---
+++ src/components/molecules/issues_tabs/IssuesTabs.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('./stylesheets/issues_tabs.scss');
+
+class IssuesTabs extends React.Component {
+  render() {
+    return (
+      <div className='issues-tabs-component'>
+        Please edit src/components/molecules/issues_tabs/IssuesTabs.js to update this component!
+      </div>
+    );
+  }
+}
+
+IssuesTabs.displayName = 'MoleculeIssuesTabs';
+
+// Uncomment properties you need
+// IssuesTabs.propTypes = {};
+// IssuesTabs.defaultProps = {};
+
+export default IssuesTabs;
