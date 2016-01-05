c407161b79a97a22168751f8ce2f54fe6ce97f14
Cassio Cabral <cassioscabral@gmail.com>
2016-01-04 23:37:39 -0300

    make vote change when clicked :heart:

--- src/components/atoms/vote_up/VoteUp.js
+++ src/components/atoms/vote_up/VoteUp.js
@@ -6,11 +6,21 @@ import FontIcon from 'material-ui/lib/font-icon';
 require('./stylesheets/vote_up.scss');
 
 class VoteUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: false, color: 'grey'};
+  }
+
+  handleClick(event) {
+    this.setState({active: !this.state.active});
+    this.setState({color: this.state.active ? 'rgb(75, 242, 7)' : 'grey'});
+  }
+
   render() {
     return (
-      <div className='vote-up-component'>
+      <div className='vote-up-component' onClick={this.handleClick.bind(this)}>
         <FontIcon className='material-icons'
-                  color='grey'>
+                  color={this.state.color}>
         arrow_drop_up
         </FontIcon>
       </div>
