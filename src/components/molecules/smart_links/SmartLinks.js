c3ad86e2e9cd55b44a6b3315f7e25a70a0fa67b5
Cassio Cabral <cassioscabral@gmail.com>
2016-02-05 22:32:06 -0300

    add auth0lock on smartlinks

--- src/components/molecules/smart_links/SmartLinks.js
+++ src/components/molecules/smart_links/SmartLinks.js
@@ -5,11 +5,21 @@ import React from 'react';
 require('./stylesheets/smart_links.scss');
 
 class SmartLinks extends React.Component {
+  componentWillMount() {
+    this.lock = new Auth0Lock('wrongAuth', 'rateissues.auth0.com');
+  }
+  showLock() {
+    // We receive lock from the parent component in this case
+    // If you instantiate it in this component, just do this.lock.show()
+    this.lock.show();
+  }
   render() {
     return (
       <div className='smart-links-component'>
         <a href='#'>about</a>
         <a href='#'>help</a>
+        <a onClick={this.showLock.bind(this)}>Sign In</a>
+
       </div>
     );
   }
