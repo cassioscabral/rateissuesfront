f54d7c5a0324900df1b8996ab09b86488cf97554
Cassio Cabral <cassioscabral@gmail.com>
2016-02-15 15:07:10 -0300

    fix github md formatting, make the issue expandable

--- src/components/molecules/issue/Issue.js
+++ src/components/molecules/issue/Issue.js
@@ -3,20 +3,56 @@
 import React from 'react';
 import CounterBox from 'components/molecules/counter_box/CounterBox';
 import Arrows from 'components/molecules/arrows/Arrows';
-
+import marked from 'marked';
+import classNames from 'classnames';
 require('./stylesheets/issue.scss');
 
 class Issue extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {active:  false};
+  }
+  rawMarkup(sanitizedHTMLAfterMarked) {
+    return {__html: sanitizedHTMLAfterMarked};
+  }
+  parseMarkdown(text) {
+    // Default values
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false
+    });
+    return marked(text);
+  }
+  getRepoName(repoUrl) {
+    let splittedUrl = repoUrl.split('/');
+
+    return `${splittedUrl[splittedUrl.length - 1]}: `;
+  }
+  toggleIssue() {
+    console.log('toggling');
+    this.setState({active: !this.state.active});
+  }
   render() {
-    var issue = this.props;
+    var issue = this.props.issue;
+    var issueClasses = classNames({
+     'issue-component': true,
+     'row': true,
+     'active': this.state.active
+   });
     return (
-      <div className='issue-component row'>
+      <div className={issueClasses}>
         <div className='description column'>
           <header>
-            <h4>{issue.title}</h4>
+            <h4 onClick={this.toggleIssue.bind(this)}><b>{this.getRepoName(issue.repository_url)}</b>{issue.title}</h4>
           </header>
-          <div className='summary'>
-            {issue.description}
+          <div className='summary'
+            dangerouslySetInnerHTML={this.rawMarkup(marked(issue.body || ''))}>
           </div>
         </div>
       </div>
--- src/components/molecules/issues_list/IssuesList.js
+++ src/components/molecules/issues_list/IssuesList.js
@@ -3,78 +3,39 @@
 import React from 'react';
 import Issue from 'components/molecules/issue/Issue';
 import Github from 'github-api';
+
 require('./stylesheets/issues_list.scss');
 
 class IssuesList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {issues:  [], teste: '123', issueTitle: '', issue: ''};
+    this.state = {issues:  []};
   }
 
   componentDidMount() {
     let github = new Github({});
-    let search = github.getSearch('language:javascript&sort=stars&order=desc');
+    let search = github.getSearch('language:ruby&sort=stars&order=desc');
     var that = this;
 
     search.issues(null, function(err, issues) {
-      // that.setState({issues: [{'a': '1'}]});
-      // that.setState((state) => { issues: state.issues.push(issues.items); });
-      that.setState({
-        issueTitle: issues.items[0].title
-      });
-      //
-      // var update = React.addons.update;
-      // var afterUpdate = update(that.state.issue, {
-      //   issue: {$set: issues.items[0]}
-      // });
-      // this.setState({issue: afterUpdate});
-      //TODO add all at once
-      issues.items.forEach((issue) => {
-        console.log(issue.title);
-        var oldIssues = that.state.issues;
-        oldIssues.push(issue);
-        that.setState({
-          issues: oldIssues
-        });
-      });
-      // var oldIssues = that.state.issues;
-      // oldIssues.concat(issues.items);
-      // that.setState({
-      //   issues: oldIssues
-      // });
+      var oldIssues = that.state.issues;
 
+      oldIssues.push(...issues.items);
+      that.setState({
+        issues: oldIssues
+      });
+      console.log(oldIssues[0]);
+      console.log(oldIssues[1]);
     });
 
   };
 
   render() {
-    // var issues = [
-    //   {
-    //     title: 'Issue title explaining the problem',
-    //     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor eveniet ex accusamus officia ullam veritatis ut qui exercitationem, ipsam fugit magnam laboriosam, assumenda tempore cumque quis placeat dicta sed. Voluptas.',
-    //     stats: {
-    //       numVotes: 2,
-    //       numComments: 10,
-    //       difficulty: 5
-    //     }
-    //   },
-    //   {
-    //     title: 'Something problematic for this project',
-    //     description: 'I don\'t have enough cats here, what do I do?',
-    //     stats: {
-    //       numVotes: 5,
-    //       numComments: 3,
-    //       difficulty: 3
-    //     }
-    //   }
-    // ];
-    // var issues = this.getIssues();
     return (
       <div className='issues-list-component'>
         {this.state.issues.map(issue => <Issue
           key={issue.id}
-          title={issue.title}
-          description={issue.body}
+          issue={issue}
          />)
         }
       </div>
