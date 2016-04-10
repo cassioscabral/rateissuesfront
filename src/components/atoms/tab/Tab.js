2f793960128c98139d4408ac46dcade5a6dc4798
Cassio Cabral <cassioscabral@gmail.com>
2016-04-10 00:48:46 -0300

    fix lint errors

--- src/components/atoms/tab/Tab.js
+++ src/components/atoms/tab/Tab.js
@@ -1,30 +1,30 @@
-'use strict';
-var classNames = require('classnames');
+'use strict'
+var classNames = require('classnames')
 
-import React from 'react';
-require('./stylesheets/tab.scss');
+import React from 'react'
+require('./stylesheets/tab.scss')
 
 class Tab extends React.Component {
   alterate() {
-    this.props.onClick(this.props.name);
+    this.props.onClick(this.props.name)
   }
   render() {
     let tabClass = classNames({
       'tab-component': true,
       'active': this.props.activeTab == this.props.name
-    });
+    })
     return (
       <div className={tabClass} onClick={this.alterate.bind(this)}>
         <span>{this.props.name}</span>
       </div>
-    );
+    )
   }
 }
 
-Tab.displayName = 'AtomTab';
+Tab.displayName = 'AtomTab'
 
 // Uncomment properties you need
 // Tab.propTypes = {};
 // Tab.defaultProps = {};
 
-export default Tab;
+export default Tab
