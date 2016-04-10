284d9d1697739670dd9844b17292c2ab22fe6306
Cassio Cabral <cassioscabral@gmail.com>
2016-04-10 01:16:24 -0300

    fix lint errors

--- src/components/molecules/issues_list/IssuesList.js
+++ src/components/molecules/issues_list/IssuesList.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import Issue from 'components/molecules/issue/Issue';
+import React from 'react'
+import Issue from 'components/molecules/issue/Issue'
 
-require('./stylesheets/issues_list.scss');
+require('./stylesheets/issues_list.scss')
 
 class IssuesList extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
   render() {
     return (
@@ -18,14 +18,14 @@ class IssuesList extends React.Component {
          />)
         }
       </div>
-    );
+    )
   }
 }
 
-IssuesList.displayName = 'MoleculeIssuesList';
+IssuesList.displayName = 'MoleculeIssuesList'
 
 // Uncomment properties you need
 // IssuesList.propTypes = {};
 // IssuesList.defaultProps = {};
 
-export default IssuesList;
+export default IssuesList
