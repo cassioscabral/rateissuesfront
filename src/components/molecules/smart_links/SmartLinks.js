a2e7922d7ad016c0d144c7e6784772648370981b
Cassio Cabral <cassioscabral@gmail.com>
2016-04-11 22:57:11 -0300

    fix lint errors

--- src/components/molecules/smart_links/SmartLinks.js
+++ src/components/molecules/smart_links/SmartLinks.js
@@ -1,52 +1,52 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import Settings from 'components/atoms/settings/Settings';
+import React from 'react'
+import Settings from 'components/atoms/settings/Settings'
 
-require('./stylesheets/smart_links.scss');
-const API_KEY = require('./API_KEY.js').API_KEY; // File is ignored by git
+require('./stylesheets/smart_links.scss')
+const API_KEY = require('./API_KEY.js').API_KEY // File is ignored by git
 
 class SmartLinks extends React.Component {
   componentWillMount() {
-    this.lock = new Auth0Lock(API_KEY, 'rateissues.auth0.com');
-    this.setState({idToken: this.getIdToken()});
+    this.lock = new Auth0Lock(API_KEY, 'rateissues.auth0.com')
+    this.setState({idToken: this.getIdToken()})
   }
   componentDidMount() {
     // In this case, the lock and token are retrieved from the parent component
     // If these are available locally, use `this.lock` and `this.idToken`
     this.lock.getProfile(this.state.idToken, function(err, profile) {
       if (err) {
-        console.log('Error loading the Profile', err);
-        return;
+        console.log('Error loading the Profile', err)
+        return
       }
-      this.setState({profile: profile});
-      localStorage.setItem('userProfile', JSON.stringify(profile));
+      this.setState({profile: profile})
+      localStorage.setItem('userProfile', JSON.stringify(profile))
 
-    }.bind(this));
+    }.bind(this))
   }
   getIdToken() {
-    var idToken = localStorage.getItem('userToken');
-    var authHash = this.lock.parseHash(window.location.hash);
+    var idToken = localStorage.getItem('userToken')
+    var authHash = this.lock.parseHash(window.location.hash)
     if (!idToken && authHash) {
       if (authHash.id_token) {
-        idToken = authHash.id_token;
-        localStorage.setItem('userToken', authHash.id_token);
+        idToken = authHash.id_token
+        localStorage.setItem('userToken', authHash.id_token)
       }
       if (authHash.error) {
-        console.log('Error signing in', authHash);
-        return null;
+        console.log('Error signing in', authHash)
+        return null
       }
     }
-    return idToken;
+    return idToken
   }
   showLock() {
     // We receive lock from the parent component in this case
     // If you instantiate it in this component, just do this.lock.show()
-    this.lock.show();
+    this.lock.show()
   }
   logout() {
-    localStorage.removeItem('userToken');
-    this.setState({idToken: null});
+    localStorage.removeItem('userToken')
+    this.setState({idToken: null})
   }
   render() {
     if (this.state.idToken) {
@@ -57,7 +57,7 @@ class SmartLinks extends React.Component {
           <a href='#'>help</a>
           <a href='#' onClick={this.logout.bind(this)}>Logout</a>
         </div>
-      );
+      )
     } else {
       return (
         <div className='smart-links-component'>
@@ -65,7 +65,7 @@ class SmartLinks extends React.Component {
           <a href='#'>help</a>
           <a href='#' onClick={this.showLock.bind(this)}>Sign In</a>
         </div>
-      );
+      )
     }
   }
 }
