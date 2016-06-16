077065b3c49d09dd0f55bee2e260526a8d4fc44d
Ed Dominike <ediko.dominike@gmail.com>
2016-06-15 23:29:27 -0300

    Remove all

--- src/config/dist.js
+++
@@ -1,8 +0,0 @@
-import baseConfig from './base'
-
-
-let config = {
-  appEnv: 'dist'  // feel free to remove the appEnv property here
-}
-
-export default Object.freeze(Object.assign({}, baseConfig, config))
