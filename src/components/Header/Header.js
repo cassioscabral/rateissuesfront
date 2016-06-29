8c78b37af23fd12b9159d706667734f010770a35
Ed Dominike <ediko.dominike@gmail.com>
2016-06-29 00:14:14 -0300

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,23 +14,12 @@ import s from './Header.scss'
 import Link from '../Link'
 import Navigation from '../Navigation'
 import LanguageSwitcher from '../LanguageSwitcher'
-import logoUrl from './logo-small.png'
 
 const messages = defineMessages({
   brand: {
     id: 'header.brand',
-    defaultMessage: 'Your Company Brand',
+    defaultMessage: 'Rate Issues',
     description: 'Brand name displayed in header'
-  },
-  bannerTitle: {
-    id: 'header.banner.title',
-    defaultMessage: 'React',
-    description: 'Title in page header'
-  },
-  bannerDesc: {
-    id: 'header.banner.desc',
-    defaultMessage: 'Complex web apps made easy',
-    description: 'Description in header'
   }
 })
 
@@ -38,30 +27,17 @@ function Header () {
   return (
     <div className={ s.root }>
       <div className={ s.container }>
-        <Navigation className={ s.nav } />
         <Link
           className={ s.brand }
           to="/"
-        >
-          <img
-            src={ logoUrl }
-            width="38"
-            height="38"
-            alt="React"
-          />
+          >
           <span className={ s.brandTxt }>
             <FormattedMessage { ...messages.brand } />
           </span>
         </Link>
-        <LanguageSwitcher />
-        <div className={ s.banner }>
-          <h1 className={ s.bannerTitle }>
-            <FormattedMessage { ...messages.bannerTitle } />
-          </h1>
-          <FormattedMessage
-            tagName="p"
-            { ...messages.bannerDesc }
-          />
+        <div className={ s.linkHolder }>
+          <LanguageSwitcher />
+          <Navigation className={ s.nav } />
         </div>
       </div>
     </div>
