c1217cd2e66083837629b549a17ec0278d69d1d5
Ed Dominike <ediko.dominike@gmail.com>
2016-07-04 23:22:53 -0300

    Remove unused import

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,7 +13,6 @@ import cx from 'classnames'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Navigation.scss'
 import Link from '../Link'
-import LanguageSwitcher from '../LanguageSwitcher'
 
 const messages = defineMessages({
   about: {
