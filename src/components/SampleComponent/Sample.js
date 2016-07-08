423f213400c1b5716524d47389c9552998603d9d
Cassio Cabral <cassioscabral@gmail.com>
2016-07-07 23:57:14 -0300

    add proptype example

--- src/components/SampleComponent/Sample.js
+++ src/components/SampleComponent/Sample.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Sample.scss'
 import {defineMessages, injectIntl} from 'react-intl'
@@ -18,6 +18,10 @@ function Sample ({someProp}) {
       { messages.sampleText } { someProp }
     </div>
   )
+}
+
+Sample.propTypes = {
+  someProp: PropTypes.string.isRequired
 }
 
 export default connect(state => ({
