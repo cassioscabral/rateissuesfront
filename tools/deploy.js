9563299443741289ee65d485da3dc8246aaf6fb1
Ed Dominike <ediko.dominike@gmail.com>
2016-07-11 14:14:43 -0300

    Add heroku config

--- tools/deploy.js
+++ tools/deploy.js
@@ -15,8 +15,8 @@ import fetch from './lib/fetch'
 // For more information visit http://gitolite.com/deploy.html
 const getRemote = (slot) => ({
   name: slot || 'production',
-  url: `https://example${slot ? `-${slot}` : ''}.scm.azurewebsites.net:443/example.git`,
-  website: `http://example${slot ? `-${slot}` : ''}.azurewebsites.net`
+  url: 'https://git.heroku.com/rateissues.git',
+  website: `http://rateissues${slot ? `-${slot}` : ''}.herokuapp.com`
 })
 
 /**
