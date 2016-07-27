f37989a7b779e9627c39775cae1735e0bd3069fc
Ed Dominike <ediko.dominike@gmail.com>
2016-07-27 12:09:00 -0300

    Remove sqlite from clean

--- tools/clean.js
+++ tools/clean.js
@@ -14,7 +14,7 @@ import fs from './lib/fs'
  * Cleans up the output (build) directory.
  */
 async function clean () {
-  await del(['.tmp', 'build/*', '!build/.git', 'database.sqlite'], {dot: true})
+  await del(['.tmp', 'build/*', '!build/.git'], {dot: true})
   await fs.makeDir('build/public')
 }
 
