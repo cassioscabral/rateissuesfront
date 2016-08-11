9ef4ed9eddfa030a3997b2dd513a038f4a6a9b40
Cassio Cabral <cassioscabral@gmail.com>
2016-08-11 14:25:50 -0300

    change component to React component class

--- src/components/GithubIssuePreview/GithubIssuePreview.js
+++ src/components/GithubIssuePreview/GithubIssuePreview.js
@@ -1,16 +1,43 @@
-import React, {PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './GithubIssuePreview.scss'
 import {connect} from 'react-redux'
+import GitHub from 'github-api'
 
-
-function GithubIssuePreview ({url}) {
-  return (
-    <div className={ s.root }>
-      This is a Github Issue Preview
-      { url }
-    </div>
-  )
+// let issue = new Issue('cassioscabral/rateissuesfront')
+// TODO make this a react component and use componentDidMount for request the issue
+class GithubIssuePreview extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {issue: {}}
+  }
+  componentDidMount () {
+    console.log('componentDidMount')
+    const gh = new GitHub()
+    gh.getRateLimit().getRateLimit()
+    .then(function (resp) {
+        console.log('Limit remaining: ' + resp.data.rate.remaining)
+        // date constructor takes epoch milliseconds and we get epoch seconds
+        console.log('Reset date: ' + new Date(resp.data.rate.reset * 1000))
+    }).catch(function (error) {
+        console.log('Error fetching rate limit', error.message)
+    })
+    console.log('getting issues', gh.getIssues('cassioscabral', 'rateissuesfront'))
+    gh.getIssues('cassioscabral', 'rateissuesfront').getIssue(1)
+    .then((response) => {
+      console.log('issue 1 response', response) // response data have the issue
+      this.setState({issue: response.data})
+    })
+  }
+  render () {
+    return (
+      <div className={ s.root }>
+        *This is a Github Issue Preview*
+        <br />
+        { /* Title: { issue1.title || '' } | State: { issue1.state || '' } | User: { issue1.user.login || '' } */}
+      </div>
+    )
+  }
 }
 
 GithubIssuePreview.propTypes = {
