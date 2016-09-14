8c4edb7090ff578a08b93d6aea52dad1616a47ee
Cassio Cabral <cassioscabral@gmail.com>
2016-09-14 17:23:31 -0300

    refactor Feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,32 +1,19 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-import React, {PropTypes} from 'react'
+import React from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './Feed.scss'
 import Story from '../Story'
 import {connect} from 'react-redux'
 
-function Feed ({stories}) {
-  return (
-    <ul className={ s.stories }>
-      { stories.map((item, index) => (
-        <Story
-          key={ index }
-          story={ item }
-        />
-      )) }
-    </ul>
-  )
-}
-
-Feed.contextTypes = {setTitle: PropTypes.func.isRequired}
+const Feed = ({stories}) => (
+  <ul className={ s.stories }>
+    { stories.map((story, index) => (
+      <Story
+        key={ index }
+        story={ story }
+      />
+    )) }
+  </ul>
+)
 
 export default connect(state => ({
   stories: state.stories.stories
