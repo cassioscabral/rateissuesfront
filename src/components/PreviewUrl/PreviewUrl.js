5bc7b5676902aece3e0fb37fc1d806cb97d2476a
Cassio Cabral <cassioscabral@gmail.com>
2016-09-14 17:53:49 -0300

    add regex to match github preview links

--- src/components/PreviewUrl/PreviewUrl.js
+++ src/components/PreviewUrl/PreviewUrl.js
@@ -2,33 +2,29 @@ import React, {PropTypes, Component} from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './PreviewUrl.scss'
 import {connect} from 'react-redux'
-import URIJS from 'urijs'
 import GithubIssuePreview from '../GithubIssuePreview'
-// const messages = defineMessages({
-//   previewUrlText: {
-//     id: 'previewUrl.previewUrlText',
-//     defaultMessage: 'PreviewUrl {customText}',
-//     description: 'Preview url Description'
-//   }
-// })
+
+function isDomain (domain, url) {
+    var pat = '^https?://(?:[^/@:]*:[^/@]*@)?(?:[^/:]+\.)?' + domain + '(?=[/:]|$)'
+    var re = new RegExp(pat, 'i')
+    return re.test(url)
+}
 
 class PreviewUrl extends Component {
   constructor (props) {
     super(props)
   }
-  // TODO
   /*
     identify url website
     switch between types of url previews
     call component based on that
   */
   identifyURL (url) {
-    switch (url) {
-      case 'https://github.com/cassioscabral/rateissuesfront/issues/1': // Github issue
-        return <GithubIssuePreview url={ url }></GithubIssuePreview>
-      default:
-        return url
+    if (isDomain('github.com', url)) {
+      return <GithubIssuePreview url={ url }></GithubIssuePreview>
     }
+
+    return url
   }
   render () {
     let {url} = this.props
