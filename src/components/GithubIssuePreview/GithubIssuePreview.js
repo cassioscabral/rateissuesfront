9ee9cf6082dd4600993ffda4dbb5358c9a251299
Cassio Cabral <cassioscabral@gmail.com>
2016-09-14 17:36:22 -0300

    refactor GithubIssuePreview

--- src/components/GithubIssuePreview/GithubIssuePreview.js
+++ src/components/GithubIssuePreview/GithubIssuePreview.js
@@ -1,20 +1,15 @@
 import React, {Component, PropTypes} from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './GithubIssuePreview.scss'
-import {connect} from 'react-redux'
 import GitHub from 'github-api'
 
-// let issue = new Issue('cassioscabral/rateissuesfront')
-// TODO make this a react component and use componentDidMount for request the issue
 class GithubIssuePreview extends Component {
   constructor (props) {
     super(props)
     this.state = {issue: null}
   }
-  componentWillMount () {
-    console.log('componentDidMount')
-    const gh = new GitHub()
-    gh.getRateLimit().getRateLimit()
+  getGithubRequestLimit (githubInstance) {
+    githubInstance.getRateLimit().getRateLimit()
     .then(function (resp) {
         console.log('Limit remaining: ' + resp.data.rate.remaining)
         // date constructor takes epoch milliseconds and we get epoch seconds
@@ -22,8 +17,15 @@ class GithubIssuePreview extends Component {
     }).catch(function (error) {
         console.log('Error fetching rate limit', error.message)
     })
-    console.log('getting issues', gh.getIssues('cassioscabral', 'rateissuesfront'))
-    gh.getIssues('cassioscabral', 'rateissuesfront').getIssue(1)
+  }
+  componentWillMount () {
+    const gh = new GitHub()
+
+    this.getGithubRequestLimit(gh)
+
+    gh
+    .getIssues('cassioscabral', 'rateissuesfront')
+    .getIssue(1)
     .then((response) => {
       console.log('issue 1 response', response) // response data have the issue
       this.setState({issue: response.data})
@@ -74,22 +76,7 @@ class GithubIssuePreview extends Component {
 }
 
 GithubIssuePreview.propTypes = {
-  // JS primitive: array  bool  func  number  object  string  symbol
-  // React element: element
-  // more: https://facebook.github.io/react/docs/reusable-components.html#prop-validation
-
-  // simple example
   url: PropTypes.string.isRequired
 }
 
-function mapStateToProps (state) {
-  return {
-  }
-}
-
-function mapDispatchToProps () {
-  return {
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)((withStyles(s)(GithubIssuePreview)))
+export default ((withStyles(s)(GithubIssuePreview)))
