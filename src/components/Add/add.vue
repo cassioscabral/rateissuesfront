f1692f94c6dfcbf587ae93cb155acf07b8395a5c
Cassio Cabral <cassioscabral@gmail.com>
2016-09-29 15:30:14 -0300

    change the folder name to third-party to avoid GH Linguist to treat Jeet as my own CSS

--- src/components/Add/add.vue
+++ src/components/Add/add.vue
@@ -21,7 +21,7 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-@import '../../assets/third-parties/jeet'
+@import '../../assets/third-party/jeet'
 .add-component
   center(960px)
 </style>
