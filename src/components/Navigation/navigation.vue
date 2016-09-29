a4a19e96820e9e6af0c230e7ba1f89580fd15478
Cassio Cabral <cassioscabral@gmail.com>
2016-09-29 16:40:36 -0300

    change navigation style to look a little better after removing bootstrap

--- src/components/Navigation/navigation.vue
+++ src/components/Navigation/navigation.vue
@@ -1,28 +1,28 @@
 <template>
-  <nav class="navbar navbar-inverse navbar-static-top">
-      <div class="navbar-header">
-          <a class="navbar-brand" v-link="{ path: '/', exact: true }">
-              <img alt="Brand" src="~assets/images/logo.png" height="20">
-          </a>
-      </div>
-      <ul class="nav navbar-nav">
-          <li v-link-active>
-              <a v-link="{ path: '/', exact: true }">Home</a>
-          </li>
-          <li v-link-active>
-              <a v-link="{ path: '/posts' }">Posts</a>
-          </li>
-          <li v-link-active>
-              <a v-link="{ path: '/add' }">Add</a>
-          </li>
-          <li v-link-active>
-              <a v-link="{ path: '/search' }">Search</a>
-          </li>
-          <li v-link-active>
-              <a v-link="{ path: '/explore' }">Explore</a>
-          </li>
-      </ul>
-  </nav>
+<nav class="navbar navbar-static-top">
+  <ul class="nav navbar-nav">
+      <li class="brand">
+        <a v-link="{ path: '/', exact: true }">
+            <img alt="Brand" src="~assets/images/logo.png" height="20">
+        </a>
+      </li>
+      <li v-link-active>
+          <a v-link="{ path: '/', exact: true }">Home</a>
+      </li>
+      <li v-link-active>
+          <a v-link="{ path: '/posts' }">Posts</a>
+      </li>
+      <li v-link-active>
+          <a v-link="{ path: '/add' }">Add</a>
+      </li>
+      <li v-link-active>
+          <a v-link="{ path: '/search' }">Search</a>
+      </li>
+      <li v-link-active>
+          <a v-link="{ path: '/explore' }">Explore</a>
+      </li>
+  </ul>
+</nav>
 </template>
 
 <script>
@@ -37,5 +37,41 @@ export default {
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+nav {
+  display: flex
+  width: 100%
+  justify-content: center
+  align-items: center
+}
+.brand {
+  padding-right: 10%
+}
+ul {
+  display: flex
+  width: 100%
+  justify-content: space-between
+  align-items: center
+  flex-direction: row
+  background: rgb(68, 54, 134)
+  li {
+    display: flex
+    min-width: 100px
+    min-height: 30px
+    text-transform: uppercase
+    justify-content: center
+    align-items: center
+    &.active {
+      background: rgb(65, 6, 139)
+      a {
+        color: rgb(242, 242, 242)
+      }
+    }
+    a {
+      display: flex
+      color: rgb(190, 190, 190)
+      text-decoration: none
+    }
+  }
+}
 </style>
