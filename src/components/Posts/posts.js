85aef9ca0790d1327ed296942ece05164ded6830
Ed Dominike <ediko.dominike@gmail.com>
2016-09-29 21:14:26 -0300

    Change to firebase at posts

--- src/components/Posts/posts.js
+++ src/components/Posts/posts.js
@@ -1,25 +1,20 @@
 import Vue from 'vue'
-import template from './posts.html'
+import {db} from 'src/helpers/firebase'
 
-import {postsResource} from 'src/helpers/resources'
+import template from './posts.html'
 
 export default Vue.extend({
   template,
 
   data () {
     return {
-      posts: []
+      posts:[]
     }
   },
 
   route: {
-    data (){
-      return postsResource.get().then((response) => {
-        return this.$set('posts', response.data)
-      }, (errorResponse) => {
-        // Handle error...
-        console.log('API responded with:', errorResponse.status)
-      })
+    data () {
+      this.$bindAsArray('posts', db.ref('posts'))
     }
   }
 })
