b58f698e06af68ffa332461df08914f0f323b700
Ed Dominike <ediko.dominike@gmail.com>
2016-09-29 22:19:19 -0300

    Add post to firebase

---
+++ src/components/Add/add.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import {db} from 'src/helpers/firebase'
+
+import template from './add.html'
+
+export default Vue.extend({
+  template,
+
+  data () {
+    return {
+      title: null,
+      body: null
+    }
+  },
+
+  firebase: {
+    posts: db.ref('posts')
+  },
+
+  route: {
+    data () {
+    }
+  },
+
+  methods: {
+    addPost (title, body) {
+      this.$firebaseRefs.posts.push({
+        title,
+        body
+      })
+    }
+  }
+})
