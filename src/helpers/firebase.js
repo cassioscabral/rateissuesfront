c8aaecadc88b8834fb68e0a5ae49edcf86bc561d
Ed Dominike <ediko.dominike@gmail.com>
2016-09-29 21:47:03 -0300

    change to singlequote

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -5,11 +5,11 @@ import Firebase from 'firebase'
 Vue.use(VueFire)
 
 let firebaseApp = Firebase.initializeApp({
-    apiKey: "AIzaSyBnrMHd4NrB3ldmvtvWBp3dnRY8BJjTOpE",
-    authDomain: "rateissue.firebaseapp.com",
-    databaseURL: "https://rateissue.firebaseio.com",
-    storageBucket: "rateissue.appspot.com",
-    messagingSenderId: "442562743753"
+    apiKey: 'AIzaSyBnrMHd4NrB3ldmvtvWBp3dnRY8BJjTOpE',
+    authDomain: 'rateissue.firebaseapp.com',
+    databaseURL: 'https://rateissue.firebaseio.com',
+    storageBucket: 'rateissue.appspot.com',
+    messagingSenderId: '442562743753'
   })
 let db = firebaseApp.database()
 
