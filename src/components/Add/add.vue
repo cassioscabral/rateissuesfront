f00af1ad037164603c01428ac29d1a2be5cbbe4b
Cassio Cabral <cassioscabral@gmail.com>
2016-09-30 18:59:56 -0300

    remove jeet

--- src/components/Add/add.vue
+++ src/components/Add/add.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="add-component">
-    <h1>Add a project on .Vue</h1>
-
+    <h1>Add a project</h1>
     <div class="control">
-      <input type="text">
-      <button type="button" name="button">Add</button>
+      <input type="text" placeholder="Find a project">
+    </div>
+    <div class="results">
+      <h3>Results</h3>
     </div>
   </div>
 </template>
@@ -21,7 +22,28 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-@import '../../assets/third-party/jeet'
-.add-component
-  center(960px)
+
+.add-component {
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+  flex-direction: column
+}
+h1 {
+  display: flex
+  font-size: 3rem
+  margin: 2rem 0
+}
+.control {
+  display: flex
+  input {
+    font-size: 2rem
+  }
+}
+
+.results {
+  display: flex
+  flex-direction: column
+}
 </style>
