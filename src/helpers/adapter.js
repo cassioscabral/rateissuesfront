386382ce1de77b177de5d806ad7d5e74cb988fbf
Ed Dominike <ediko.dominike@gmail.com>
2016-10-04 19:22:40 -0300

    Add js-data and firebase

---
+++ src/helpers/adapter.js
@@ -0,0 +1,20 @@
+import {FirebaseAdapter} from 'js-data-firebase'
+import firebase from 'firebase'
+
+const dataSource = 'firebase'
+
+let config = {
+  apiKey: 'AIzaSyBnrMHd4NrB3ldmvtvWBp3dnRY8BJjTOpE',
+  authDomain: 'rateissue.firebaseapp.com',
+  databaseURL: 'https://rateissue.firebaseio.com',
+  storageBucket: 'rateissue.appspot.com',
+  messagingSenderId: '442562743753'
+}
+firebase.initializeApp(config)
+
+const adapter = new FirebaseAdapter({
+  db: firebase.database()
+})
+
+export default adapter
+export {dataSource}
