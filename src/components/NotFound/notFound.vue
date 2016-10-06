50788be19299c72150d73550837fae31a5fac0ec
Cassio Cabral <cassioscabral@gmail.com>
2016-10-06 15:34:30 -0300

    move notFound page to .vue

---
+++ src/components/NotFound/notFound.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1 class="page-header">Not found...</h1>
+    <p class="lead">Sorry :(</p>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style>
+</style>
