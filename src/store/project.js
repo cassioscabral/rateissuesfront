f1b8af2b7b2159d903cac2483a903f8f384fa54e
Cassio Cabral <cassioscabral@gmail.com>
2016-11-11 22:47:47 -0300

    create new add project mutation

--- src/store/project.js
+++ src/store/project.js
@@ -1,7 +1,9 @@
 import {project as ProjectMapper} from 'src/helpers/store'
 import {
   ADD_PROJECTS,
-  ADD_PROJECTS_FILTERED_BY_ID} from './mutations.js'
+  ADD_PROJECTS_FILTERED_BY_ID,
+  ADD_PROJECT
+} from './mutations.js'
 
 export default {
   state: {
@@ -16,15 +18,14 @@ export default {
     [ADD_PROJECTS] (state, payload) {
       state.projects = payload.projects
     },
-    [ADD_PROJECTS_FILTERED_BY_ID] (state, payload) {
-      let projects = payload.projects.reduce((previous, current) => {
-        previous[current.id] = {...current}
-        return previous
-      },{})
+    [ADD_PROJECTS_FILTERED_BY_ID] (state, {projectsFilteredById, query}) {
       state.projectsFilteredById = {
-        query: payload.query,
-        data: projects
+        query,
+        data: projectsFilteredById
       }
+    },
+    [ADD_PROJECT] (state, {project}) {
+      state.projects = [...state.projects, project]
     }
   },
 
@@ -34,13 +35,17 @@ export default {
   },
 
   actions: {
-    findProjectsByIds ({commit}, payload) {
+    findProjectsByIds ({commit}, {ids = []}) {
       let query = {}
-      let ids = payload.ids || []
-      query.where = {id:{in: ids}}
+      query.where = {id: {in: ids}}
 
       ProjectMapper.findAll(query).then(projects => {
-        commit(ADD_PROJECTS_FILTERED_BY_ID, {query, projects})
+        // transform from array to object with id as keys
+        let projectsFilteredById = projects.reduce((previous, current) => {
+          previous[current.id] = {...current}
+          return previous
+        }, {})
+        commit(ADD_PROJECTS_FILTERED_BY_ID, {projectsFilteredById, query})
       })
     },
     findProjects ({commit}, input) {
@@ -51,6 +56,26 @@ export default {
       ProjectMapper.findAll(query).then(projects => {
         commit(ADD_PROJECTS, {projects})
       })
+    },
+    async addProject ({commit, state}, project) {
+      try {
+        let newProject = await ProjectMapper.create({
+          ...project
+        })
+        commit(ADD_PROJECT, {newProject})
+
+        // Update projectsFilteredById to reflect the projects change
+        let projectsFilteredById = {
+          ...state.projectsFilteredById.data,
+          [`${newProject.id}`]: newProject
+        }
+        commit(ADD_PROJECTS_FILTERED_BY_ID, {
+          projectsFilteredById,
+          query: state.projectsFilteredById.query
+        })
+      } catch (err) {
+        console.error(err)
+      }
     }
   }
 
