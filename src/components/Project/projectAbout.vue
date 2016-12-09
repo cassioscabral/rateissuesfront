a6b788cfb29883c486b0f4b32a8af984681772d6
Cassio Cabral <cassioscabral@gmail.com>
2016-12-09 00:11:01 -0300

    update project about layout

--- src/components/Project/projectAbout.vue
+++ src/components/Project/projectAbout.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="container" v-if="hasProject">
-    Project About
+    <div class="columns">
+      <div class="column is-4">
+        <p class="content">
+          <h2>Project: {{project.githubData.name}}</h2>
+        </p>
+
+        <p class="content">
+          <figure class="image is-128x128">
+            <img :src="project.githubData.owner.avatar_url">
+          </figure>
+        </p>
+
+        <p class="content">
+          Full Name: {{project.githubData.full_name}}
+        </p>
+
+        <p class="content">
+          Description: {{project.githubData.description}}
+        </p>
+      </div>
+      <div class="column is-4">
+        <p class="content">
+          <h2>Second Column</h2>
+        </p>
+      </div>
+      <div class="column is-4">
+        <p class="content">
+          <h2>Third Column</h2>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,6 +61,8 @@ export default {
     hasProject () {
       return !isEmpty(this.project)
     }
+  },
+  components: {
   }
 }
 </script>
